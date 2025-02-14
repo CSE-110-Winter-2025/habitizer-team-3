4e307d541fb08e3d056d7c8d2bd1bdc5dc2065ca
John Zhou <itszhoudoh@gmail.com>
2025-02-14 13:37:53 -0800

    Instrumentated tests for TimerViewModel, should add testing for MainViewModel as well

---
+++ app/src/androidTest/java/edu/ucsd/cse110/habitizer/app/TimerViewModelTest.java
@@ -0,0 +1,145 @@
+package edu.ucsd.cse110.habitizer.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class TimerViewModelTest {
+
+    @Test
+    public void testStartTimer() throws InterruptedException {
+        // Create the ViewModel
+        TimerViewModel viewModel = new TimerViewModel();
+
+        // Initially, should be 0
+        assertEquals("Initial elapsed time should be 0",
+                0, (int) viewModel.getElapsedSeconds().getValue());
+
+        // Start the timer
+        viewModel.startTimer();
+
+        // Wait at least 1 second for the timer to increment
+        Thread.sleep(1500);
+
+        assertTrue("Elapsed time should be >= 1 after ~1.5s",
+                viewModel.getElapsedSeconds().getValue() >= 1);
+
+        viewModel.stopTimer();
+    }
+
+    @Test
+    public void testStopTimer() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+        viewModel.startTimer();
+        Thread.sleep(1500);
+
+        int beforeStop = viewModel.getElapsedSeconds().getValue();
+        viewModel.stopTimer();
+        Thread.sleep(1500);
+
+        // After stopping the elapsed time should not increase
+        int afterStop = viewModel.getElapsedSeconds().getValue();
+        assertEquals("Elapsed time should not increase after stopTimer()",
+                beforeStop, afterStop);
+    }
+
+    @Test
+    public void testForwardTimer() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+
+        // Initially 0
+        assertEquals(0, (int) viewModel.getElapsedSeconds().getValue());
+
+        // Forward 30 seconds
+        viewModel.forwardTimer();
+        // Sleep to allow LiveData update
+        Thread.sleep(500);
+        assertEquals(30, (int) viewModel.getElapsedSeconds().getValue());
+    }
+
+    @Test
+    public void testGetElapsedSeconds() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+        assertEquals(0, (int) viewModel.getElapsedSeconds().getValue());
+
+        // Start timer, wait
+        viewModel.startTimer();
+        Thread.sleep(1500);
+
+        // Check if incremented
+        int elapsed = viewModel.getElapsedSeconds().getValue();
+        assertTrue("Elapsed time should be >= 1 after ~1.5s", elapsed >= 1);
+
+        viewModel.stopTimer();
+    }
+
+    @Test
+    public void testMultipleStartTimer() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+        viewModel.startTimer();
+        Thread.sleep(1500);
+        int firstValue = viewModel.getElapsedSeconds().getValue();
+        // Calling startTimer() again should not reset the timer
+        viewModel.startTimer();
+        Thread.sleep(1500);
+        int secondValue = viewModel.getElapsedSeconds().getValue();
+        // The timer should continue running and not restart from 0.
+        assertTrue("Timer should continue after calling startTimer() twice",
+                secondValue >= firstValue + 1);
+        viewModel.stopTimer();
+    }
+
+    @Test
+    public void testStopTimerWithoutStart() {
+        TimerViewModel viewModel = new TimerViewModel();
+        // Calling stopTimer() without starting should leave elapsed time at 0.
+        viewModel.stopTimer();
+        assertEquals("Elapsed time should remain 0 if timer is never started",
+                0, (int) viewModel.getElapsedSeconds().getValue());
+    }
+
+    @Test
+    public void testMultipleStopTimer() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+        viewModel.startTimer();
+        Thread.sleep(1500);
+        viewModel.stopTimer();
+        int stoppedValue = viewModel.getElapsedSeconds().getValue();
+        // Calling stopTimer() a second time should not change anything.
+        viewModel.stopTimer();
+        Thread.sleep(1500);
+        int valueAfterMultipleStops = viewModel.getElapsedSeconds().getValue();
+        assertEquals("Elapsed time should not change after multiple stopTimer() calls",
+                stoppedValue, valueAfterMultipleStops);
+    }
+
+    @Test
+    public void testMultipleForwardTimer() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+        // Call forwardTimer() twice consecutively
+        viewModel.forwardTimer();
+        Thread.sleep(500);
+        viewModel.forwardTimer();
+        Thread.sleep(500);
+        assertEquals("Elapsed time should be 60 after calling forwardTimer() twice",
+                60, (int) viewModel.getElapsedSeconds().getValue());
+    }
+
+    @Test
+    public void testForwardTimerAfterStop() throws InterruptedException {
+        TimerViewModel viewModel = new TimerViewModel();
+        viewModel.startTimer();
+        Thread.sleep(1500);
+        viewModel.stopTimer();
+        int stoppedValue = viewModel.getElapsedSeconds().getValue();
+        // Even with the timer stopped forwardTimer should add 30 to the current value.
+        viewModel.forwardTimer();
+        Thread.sleep(500);
+        assertEquals("Elapsed time should increment by 30 after forwardTimer() when timer is stopped",
+                stoppedValue + 30, (int) viewModel.getElapsedSeconds().getValue());
+    }
+}
