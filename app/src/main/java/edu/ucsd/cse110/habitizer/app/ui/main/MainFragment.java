ab35320533eef875a4d63a62139974d93c107295
Meiqi Liu <meiqi.liu31@gmail.com>
2025-03-11 22:33:04 -0700

    Being able to forward timer whether pause is pressed or not

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/main/MainFragment.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/main/MainFragment.java
@@ -392,7 +392,7 @@ public class MainFragment extends Fragment {
         if (timerViewModel.isPaused()) {
             timerViewModel.resumeTimer();
             view.pauseResumeButton.setImageResource(R.drawable.baseline_pause_24);
-            view.fastforwardButton.setEnabled(true);
+            //view.fastforwardButton.setEnabled(true);
             view.endButton.setEnabled(true);
             view.stopButton.setEnabled(true);
         } else {
@@ -400,7 +400,7 @@ public class MainFragment extends Fragment {
             view.endButton.setEnabled(false);
             view.stopButton.setEnabled(false);
             view.pauseResumeButton.setImageResource(R.drawable.baseline_play_arrow_24);
-            view.fastforwardButton.setEnabled(false);
+            //view.fastforwardButton.setEnabled(false);
         }
     }
 }
